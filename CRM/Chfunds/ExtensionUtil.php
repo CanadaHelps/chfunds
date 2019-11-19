df9bf5f7975f3e88c178edaa85bdaa4fcd72124d
Monish Deb <deb.monish@gmail.com>
2019-11-19 17:17:34 +0530

    move contribution update code to Extension util

--- CRM/Chfunds/ExtensionUtil.php
+++ CRM/Chfunds/ExtensionUtil.php
@@ -72,4 +72,41 @@ public static function updateContributions($params, $CHFund, $entity = 'Contribu
   }
 }
 
+public static function updateCHContribution($financialTypeID, $chFund) {
+  $params = [
+    'custom_' . self::getCHFundCustomID() => $chFund,
+  ];
+  $totalContributions = civicrm_api3('Contribution', 'getcount', $params);
+  if ($totalContributions > 0) {
+    $batchSize = 1000;
+    $offset = $limit = 0;
+    while ($limit < $totalContributions) {
+      $limit += $batchSize;
+      $contributions = civicrm_api3('Contribution', 'get', array_merge($params, [
+        'options' => [
+          'limit' => $limit,
+          'offset' => $offset,
+        ],
+        "return" => ["id", "financial_type_id"],
+      ]))['values'];
+      $values = [];
+      foreach ($contributions as $id => $value) {
+        if ($value['financial_type_id'] != $financialTypeID) {
+          $values[] = sprintf(
+            " ( '%s', %d, %d ) ",
+            $chFund,
+            $financialTypeID,
+            $value['id']
+          );
+        }
+      }
+      if (!empty($values)) {
+        $sql = "INSERT INTO civicrm_ch_contribution_batch(`ch_fund`, `fund`, `contribution_id`) VALUES " . implode(', ', $values) . " ;";
+        CRM_Core_DAO::executeQuery($sql);
+      }
+      $offset += $batchSize + 1;
+    }
+  }
+}
+
 }
