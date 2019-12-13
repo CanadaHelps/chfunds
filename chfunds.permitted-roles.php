757c830a623d65a71b9f60c016fa9a2b31695993
Monish Deb <deb.monish@gmail.com>
2019-12-13 16:35:55 +0530

    Change comments in chfunds.permitted-roles file

--- chfunds.permitted-roles.php
+++ chfunds.permitted-roles.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * This constant DRUPAL_ROLE_PERMISSIONS holds the drupal role and the respective with CH Permissions in serialised format
- * If you unserialize the contstant it will give following array in ['drupal-role-name' => [permission1, permission2 ...]] format,
+ * This constant DRUPAL_ROLE_PERMISSIONS holds an array of drupal roles and permissions that are to be programmatically assigned to each role in serialised format
+ * If you unserialize the constant it will give following array in ['drupal-role-name' => [permission1, permission2 ...]] format,
  * where 'drupal-role-name' is case insensitive but permissions aren't
  *
  * unserialize(DRUPAL_ROLE_PERMISSIONS) = [
@@ -12,9 +12,9 @@
  *  ];
  *
  * which means 'adminstrator' role is given 'CH admin miscellaneous' and 'assign CH Fund' permission.
- * You can change it to whichever role(s) you like to assign permission with. Like if you want to give
- * 'CH admin miscellaneous' permission to 'adminstrator' role and 'assign CH Fund' to 'staff adminstrator' role
- * then the array would be:
+ * You can change it to assigning whichever permissions you want to whichever roles you want, assuming
+ * permissions and roles exist. Like if you want to give 'CH admin miscellaneous' permission to 'adminstrator'
+ * role and 'assign CH Fund' to 'staff adminstrator' role then the array would be:
  * [
  *   'adminstrator' => [
  *     'CH admin miscellaneous',
